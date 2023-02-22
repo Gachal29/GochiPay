71e7c8e61d35854e2d585f4370c89f7de63c2463
Gachal29 <taisuke0129t@gmail.com>
2023-02-22 10:44:11 +0900

    一時コミット

--- gochipay/shop/urls.py
+++ gochipay/shop/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PaymentView, PaidView
+from .views import PaymentView, PaidView, WriteView, ReviewView
 
 urlpatterns = [
     path("", PaymentView.as_view(), name="shop"),
-    path("paid", PaidView.as_view(), name="paid")
+    path("paid", PaidView.as_view(), name="paid"),
+    path("write", WriteView.as_view(), name="write"),
+    path("review", ReviewView.as_view(), name="review")
 ]
